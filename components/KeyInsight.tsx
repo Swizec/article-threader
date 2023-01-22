39287c1e3c891d9ff14f766ed01ab7336fd27b69
Swizec Teller <swizec@swizec.com>
2023-01-22 15:31:14 -0800

    Make a mini tldr tool

---
+++ components/KeyInsight.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { Box, Button, Flex, Input, Label, Paragraph, Spinner } from "theme-ui";
+
+async function findKeyInsight(vars: { url: string }) {
+    const res = await fetch("/api/findKeyInsight", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vars),
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to find key insight");
+    }
+
+    return res.text();
+}
+
+const Thread = (props: { text: string }) => {
+    const text = props.text.replaceAll(/(\n)+/g, "\n\n");
+
+    return <Paragraph>{text}</Paragraph>;
+};
+
+export const KeyInsight = () => {
+    const [url, setUrl] = useState<string>("");
+
+    const {
+        data: keyInsight,
+        isLoading,
+        mutateAsync,
+    } = useMutation(findKeyInsight);
+
+    async function makeThread(event: React.FormEvent) {
+        event.preventDefault();
+
+        if (url) {
+            await mutateAsync({ url });
+        }
+    }
+
+    return (
+        <Box sx={{ textAlign: "center" }}>
+            <h1>What is the point?</h1>
+            <Paragraph>
+                Reading? Ain't nobody got time for that. Paste the URL, get the
+                point 👇
+            </Paragraph>
+            <Flex
+                as="form"
+                onSubmit={makeThread}
+                sx={{
+                    p: 1,
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Input
+                    name="url"
+                    value={url}
+                    onChange={(e) => setUrl(e.currentTarget.value)}
+                    sx={{ m: 3, width: "100%" }}
+                />
+
+                {isLoading ? (
+                    <Spinner />
+                ) : (
+                    <Button type="submit" sx={{ cursor: "pointer" }}>
+                        What is the point?
+                    </Button>
+                )}
+            </Flex>
+
+            {keyInsight ? (
+                <>
+                    <h2>The point 👇</h2>
+                    <Paragraph sx={{ m: "auto", p: 2 }}>{keyInsight}</Paragraph>
+                </>
+            ) : null}
+        </Box>
+    );
+};
---
+++ pages/api/findKeyInsight.ts
@@ -0,0 +1,49 @@
+import { Configuration, OpenAIApi } from "openai";
+import { extract } from "@extractus/article-extractor";
+import { stripHtml } from "string-strip-html";
+import { NextApiHandler } from "next";
+
+const openai = new OpenAIApi(
+    new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    })
+);
+
+const findKeyInsight: NextApiHandler = async (req, res) => {
+    const { url } = req.body;
+
+    const article = await extract(url);
+
+    if (!article.title || !article.content) {
+        return res.send(400);
+    }
+
+    const cleanContent = stripHtml(article.content).result;
+
+    console.log(cleanContent);
+
+    const insight1 = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `What is the most surprising insight in this article: ${cleanContent}`,
+        temperature: 0.6,
+        max_tokens: 800,
+    });
+
+    // const insight2 = await openai.createCompletion({
+    //     model: "text-davinci-003",
+    //     prompt: `Turn this description into a viral tweet: ${insight1.data.choices[0].text.trim()}`,
+    //     temperature: 0.6,
+    //     max_tokens: 800,
+    // });
+
+    res.status(200).send(insight1.data.choices[0].text);
+
+    // res.status(200).send(
+    //     [
+    //         insight1.data.choices[0].text.trim(),
+    //         insight2.data.choices[0].text.trim(),
+    //     ].join("\n\n")
+    // );
+};
+
+export default findKeyInsight;
