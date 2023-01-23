23496b26439a5dec82a16ab3c6c0a604945f8831
Swizec Teller <swizec@swizec.com>
2023-01-22 16:12:33 -0800

    Fix plausible loading?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
@@ -9,7 +10,14 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
             <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
+                <>
+                    <Script
+                        data-domain="whatisthepoint.xyz"
+                        src="https://plausible.io/js/script.js"
+                    />
+
+                    <Component {...pageProps} />
+                </>
             </QueryClientProvider>
         </ThemeProvider>
     );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <Script
-                data-domain="whatisthepoint.xyz"
-                src="https://plausible.io/js/script.js"
-            />
+
             <body>
                 <Main />
                 <NextScript />
