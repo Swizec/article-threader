f0c431104b07bfcf26c29097c6d20cbbd091a986
Swizec Teller <swizec@swizec.com>
2023-02-05 17:06:08 -0800

    Add extension support, I think

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,22 +55,22 @@ export default function Home() {
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="/apple-touch-icon.png"
+                    href="/icons/apple-touch-icon.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/favicon-32x32.png"
+                    href="/icons/favicon-32x32.png"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/favicon-16x16.png"
+                    href="/icons/favicon-16x16.png"
                 />
                 <link rel="manifest" href="/site.webmanifest" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/icons/favicon.ico" />
             </Head>
 
             <Container
