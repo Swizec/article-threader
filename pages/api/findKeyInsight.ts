073d9cc9278cf25c16c5392af5c17613217f4bb3
Swizec Teller <swizec@swizec.com>
2023-02-05 16:08:02 -0800

    Cache whatispoint results

--- pages/api/findKeyInsight.ts
+++ pages/api/findKeyInsight.ts
@@ -2,6 +2,8 @@ import { Configuration, OpenAIApi } from "openai";
 import { extract } from "@extractus/article-extractor";
 import { stripHtml } from "string-strip-html";
 import { NextApiHandler } from "next";
+import { Redis } from "@upstash/redis";
+import { URL } from "url";
 
 const openai = new OpenAIApi(
     new Configuration({
@@ -9,13 +11,40 @@ const openai = new OpenAIApi(
     })
 );
 
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL!,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
+
+// remove utm params, the most likely for us to be useless
+function getCleanUrl(url: string): string {
+    const parsed = new URL(url);
+
+    for (const param of Array.from(parsed.searchParams.keys())) {
+        if (param.startsWith("utm_")) {
+            parsed.searchParams.delete(param);
+        }
+    }
+
+    return parsed.toString();
+}
+
 const findKeyInsight: NextApiHandler = async (req, res) => {
     const { url } = req.body;
+    const cleanUrl = getCleanUrl(url);
 
-    const article = await extract(url);
+    console.log(cleanUrl);
+
+    const cached = await redis.get(cleanUrl);
+
+    if (cached) {
+        return res.status(200).send(cached);
+    }
+
+    const article = await extract(cleanUrl);
 
     if (!article.title || !article.content) {
-        return res.send(400);
+        return res.status(400).send("Couldn't extract article");
     }
 
     const cleanContent = stripHtml(article.content).result;
@@ -34,14 +63,11 @@ const findKeyInsight: NextApiHandler = async (req, res) => {
     //     max_tokens: 800,
     // });
 
-    res.status(200).send(insight1.data.choices[0].text);
+    const finalResult = insight1.data.choices[0].text;
 
-    // res.status(200).send(
-    //     [
-    //         insight1.data.choices[0].text.trim(),
-    //         insight2.data.choices[0].text.trim(),
-    //     ].join("\n\n")
-    // );
+    await redis.set(cleanUrl, finalResult);
+
+    res.status(200).send(finalResult);
 };
 
 export default findKeyInsight;
