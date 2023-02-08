de1acbc3045f15c74a0c99f9ad6ca1b021c84629
Swizec Teller <swizec@swizec.com>
2023-02-08 08:58:49 -0800

    Fix homepage demo

--- components/KeyInsight.tsx
+++ components/KeyInsight.tsx
@@ -4,7 +4,7 @@ import { Box, Button, Flex, Input, Link, Paragraph, Spinner } from "theme-ui";
 import { usePlausible } from "next-plausible";
 
 export async function findKeyInsight(vars: { url: string }) {
-    const res = await fetch("https://whatispoint.xyz/api/findKeyInsight", {
+    const res = await fetch("/api/findKeyInsight", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
